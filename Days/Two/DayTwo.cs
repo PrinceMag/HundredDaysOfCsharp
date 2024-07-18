cd55e53a3636778442e487c549da0f5ee5fdb926
Prince Magutshwa <princeth.magutshwa@gmail.com>
2024-07-18 15:52:54 +0200

    Day 02 of 100 days of C# on Leetcode

---
+++ Days/Two/DayTwo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HundredDaysOfCsharp.Days.Two
+{
+    public class DayTwo
+    {
+        public int FindTheWinner(int n, int k)
+        {
+            var peopleList = new List<int>(Enumerable.Range(1, n));
+
+            int currentIndex = 0;
+            while (peopleList.Count > 1)
+            {
+                currentIndex = (currentIndex + k - 1) % peopleList.Count;
+                peopleList.RemoveAt(currentIndex);
+            }
+
+            return peopleList[0];
+        }
+    }
+}
